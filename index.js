dc93ba7f5564b6d5d67b4be3e08363e315cd06f5
Joshua <117291896+devstygian@users.noreply.github.com>
2023-09-27 06:03:58 +0800

    New Update
    Open this now

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ window.alert("Hacker ahead!") // new prog
 /*
     this is for multiple line comment
 */
+
+// Check this out!
+console.log("Hello",fisetName,"!")
+console.log("This is your age now!",age,"years old");
+console.log("Enrolled",student);
+
+document.getElementById("p1").innerHTML = "Hello " + fisetName;
+document.getElementById("p2").innerHTML = "Your age is " + age+ 'years old!';
+document.getElementById('p3').innerHTML = "Enrolled " + student+ "!"
