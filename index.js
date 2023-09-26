0d4d4f606e758a396a43f98076cbe98a0f939826
Joshua <117291896+devstygian@users.noreply.github.com>
2023-09-27 06:07:53 +0800

    Update index.js New code

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ window.alert("Hacker ahead!") // new prog
 */
 
 // Check this out!
+
 console.log("Hello",fisetName,"!")
 console.log("This is your age now!",age,"years old");
 console.log("Enrolled",student);
@@ -17,3 +18,24 @@ console.log("Enrolled",student);
 document.getElementById("p1").innerHTML = "Hello " + fisetName;
 document.getElementById("p2").innerHTML = "Your age is " + age+ 'years old!';
 document.getElementById('p3').innerHTML = "Enrolled " + student+ "!"
+
+//New Code Below
+let age=19;
+
+age +=1;
+age -=1;
+age *=2;
+age %=3;
+
+console.log(age)
+/*
+    Operator precedence
+    1. parenthesis ()
+    2. exponints
+    3. multuplication & divition
+    4. addition & subtraction
+*/
+
+let result = 1+2*(3+4);
+
+console.log(result);
