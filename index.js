61af7350d644fd3cf0ce4ece08e0d572334506eb
Joshua <117291896+devstygian@users.noreply.github.com>
2023-09-27 06:01:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.log('Hello User!') // old
 window.alert("Hacker ahead!") // new prog 
+
+// NOte: The comment are semilar in Java and CSS
+
+// this is one line comment
+
+/*
+    this is for multiple line comment
+*/
